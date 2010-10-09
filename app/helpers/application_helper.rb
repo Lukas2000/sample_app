31a034dc498417c1ed73a88d9cbbd103cbb1f2b8
Lukas2000 <syedfareedahmed@gmail.com>
2010-10-08 20:38:42 -0400

    Finished layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
-# Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-# Return a title on a per-page basis.
+
+  # Return a title on a per-page basis.
   def title
     base_title = "Ruby on Rails Tutorial Sample App"
     if @title.nil?
@@ -8,5 +8,6 @@ module ApplicationHelper
     else
       "#{base_title} | #{@title}"
     end
-  end  
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
+  map.contact '/contact', :controller => 'pages', :action => 'contact'
+  map.about   '/about',   :controller => 'pages', :action => 'about'
+  map.help    '/help',    :controller => 'pages', :action => 'help'
+  map.signup  '/signup',  :controller => 'users', :action => 'new'
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
@@ -31,7 +35,7 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-   map.root :controller => "pages", :action=> 'home'
+  map.root :controller => "pages", :action=> 'home'
 
   # See how all your routes lay out with "rake routes"
 
@@ -40,7 +44,5 @@ ActionController::Routing::Routes.draw do |map|
   # consider removing or commenting them out if you're using named routes and resources.
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
-  map.contact '/contact', :controller => 'pages', :action => 'contact'
-  map.about   '/about',   :controller => 'pages', :action => 'about'
-  map.help    '/help',    :controller => 'pages', :action => 'help'
+
 end
